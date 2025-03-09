e797f00e388ec6a25054d6801a5297854d44de13
José Everardo <joseglezsisa@gmail.com>
2025-03-08 20:36:22 -0600

    Finalizando estilos de pagina inicial

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function ocultar_imagen(){
+    document.getElementById("ocultarElementos").style.display = 'none';
+}
+
+/*ambas formas son validas para ocultar los elementos
+
+function ocultar_imagen(){
+    document.getElementById("ocultarElementos").style.display = 'none';
+}
+
+function ocultar_texto(){
+    document.getElementById("textoCuadro").style.visibility = "hidden";
+} */
