94c994a6955103fe41a55b6081e10ad4921e6397
José Everardo <joseglezsisa@gmail.com>
2025-03-12 17:40:55 -0600

    añadiendo funcioanlidad de encriptar

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
+const textoArea = document.querySelector(".mensaje-cuadrodetexto");//almacena lo que el usuario escribe
+const mensaje = document.querySelector(".texto-respuesta");
+
+/*    La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
 
 function encriptar(){
     document.getElementById("ocultarElementos").style.display = 'none';
 
-    document.getElementById('textoEncriptado').style.display = 'flex';
+    document.getElementById('respuesta').style.display = 'flex';
     document.getElementById("botonCopiar").style.display = "block";
 
-    const alumnos = document.querySelector("mensaje-cuadrodetexto");
-
-    for (let indice = 0; indice < alumnos; indice++) {
-      const nuevoTelefono = alumnos[indice].textContent.replace("a","ai");
-      alumnos[indice].textContent = nuevoTelefono;
-      console.log(nuevoTelefono);
-
-      var text = document.getElementById("texto").value;
-        var newText = text.replace("a", "ai");
-        document.getElementById("texto").value = newText;
-        console.log(newText);
-  }
+    const textoEncriptado = encriptacion(textoArea.value);
+    mensaje.value = textoEncriptado;
 }
+
+function encriptacion(stringEncriptada){
+
+
+let matrizCodigo = [["e", "entre"], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];//se crea un conjunto de arreglos o arreglo de arreglos
+stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let indice = 0; indice < matrizCodigo.length; indice++) {
+    if(stringEncriptada.includes(matrizCodigo[indice][0])){
+      stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[indice][0],matrizCodigo[indice][1])
+    }
+}
+return stringEncriptada;
+}
+
 
 /*ambas formas son validas para ocultar los elementos
 
