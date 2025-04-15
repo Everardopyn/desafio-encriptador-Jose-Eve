63e7ce5a360a68ae833a963004be81cd4ed3fbaa
José Everardo <joseglezsisa@gmail.com>
2025-04-15 14:39:54 -0600

    mejorando usabilidad

--- app.js
+++ app.js
@@ -6,37 +6,51 @@ La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"*/
-
+let bot = textoArea;
 function encriptar(){
+
+  const textoEncriptado = encriptacion(textoArea.value);
+
+  if (textoEncriptado === ""){
+    alert("Ingrese un texto")
+  }else{
+
     document.getElementById("ocultarElementos").style.display = 'none';
 
     document.getElementById('respuesta').style.display = 'flex';
     document.getElementById("botonCopiar").style.display = "block";
 
-    const textoEncriptado = encriptacion(textoArea.value);
+
     mensaje.value = textoEncriptado;
     textoArea.value = '';//limpia el campo para que no lo tengas que borrar manualmente
+  }
 }
 
 function encriptacion(stringEncriptada){
+  
 
+    let matrizCodigo = [["e", "entre"], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];//se crea un conjunto de arreglos o arreglo de arreglos
+    stringEncriptada = stringEncriptada.toLowerCase();
 
-let matrizCodigo = [["e", "entre"], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];//se crea un conjunto de arreglos o arreglo de arreglos
-stringEncriptada = stringEncriptada.toLowerCase();
-
-  for (let indice = 0; indice < matrizCodigo.length; indice++) {
-    if(stringEncriptada.includes(matrizCodigo[indice][0])){
-      stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[indice][0],matrizCodigo[indice][1]);
-    }
-}
-return stringEncriptada;
+      for (let indice = 0; indice < matrizCodigo.length; indice++) {
+        if(stringEncriptada.includes(matrizCodigo[indice][0])){
+          stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[indice][0],matrizCodigo[indice][1]);
+        }
+      }
+      return stringEncriptada;
+    
 }
 
 function Desencriptar(){
 
   const textoEncriptado = desencriptacion(textoArea.value);
+
+  if(textoEncriptado === ""){
+    alert("Ingrese un texto");
+  }else{
   mensaje.value = textoEncriptado;
   textoArea.value = '';
+  }
 }
 
 function desencriptacion(stringDEsencriptada){
